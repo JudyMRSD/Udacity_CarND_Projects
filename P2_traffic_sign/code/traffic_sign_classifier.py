ab0d9c8f664a16d315c90d4f7882195a51a0744d
Jin Zhu <jinz1@andrew.cmu.edu>
2018-04-29 12:16:08 -0400

    refactor

--- P2_traffic_sign/code/traffic_sign_classifier.py
+++ P2_traffic_sign/code/traffic_sign_classifier.py
@@ -1,11 +1,36 @@
 from LeNet import LeNet
 
-class TrafficSignClassifier():
-    num_classes = 40
-    img_width, img_height, img_channels = [32, 32, 1]
-    img_params = [img_width, img_height, img_channels]
-    # img_params = img.shape()
-    lenet = LeNet(num_classes, img_params)
-    lenet.build()
 
-tsc = TrafficSignClassifier()
+class Pipeline():
+    def __init__(self):
+        self.num_classes = 40
+        img_width, img_height, img_channels = [32, 32, 1]
+        self.img_params = [img_width, img_height, img_channels]
+        # img_params = img.shape()
+        self.lenet = LeNet(self.num_classes, self.img_params)
+        print("build")
+        self.lenet.build()
+
+    def train(self):
+        pass
+
+    def classify(self):
+        pass
+
+class ImgPreprocess():
+    def __init__(self):
+        self.a = 1
+    def imgStats(self):
+        pass
+    def preprocess(self):
+        pass
+
+def main():
+    # set input training image directory
+    # run pipeline
+    # output classification results
+    traffic_sign_pipeline = Pipeline()
+
+if __name__ == '__main__':
+    main()
+
