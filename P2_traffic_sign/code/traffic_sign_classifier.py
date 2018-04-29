f19812701709924e3af0db24ee228f450f3ac431
Jin Zhu <jinz1@andrew.cmu.edu>
2018-04-29 13:10:52 -0400

    utils visualization

--- P2_traffic_sign/code/traffic_sign_classifier.py
+++ P2_traffic_sign/code/traffic_sign_classifier.py
@@ -2,23 +2,29 @@ from LeNet import LeNet
 from utils import DataSetTools
 
 class Pipeline():
-    def __init__(self, data_dir):
+    def __init__(self, data_dir, visualize_dir):
         # img_params = img.shape()
         self.data_dir = data_dir
+        self.visualize_dir = visualize_dir
         self.exploreDataset()
         # todo: uncomment buildModel:
-        # self.lenet = LeNet(self.num_classes, self.img_params)
-        # self.lenet.build()
+        self.buildNetwork()
 
     def exploreDataset(self):
         # load data
         self.dataTool = DataSetTools()
         self.dataTool.loadData(self.data_dir)
         self.dataTool.summarizeData()
+        self.dataTool.visualizeData(tag='train', imgPath=self.visualize_dir)
         # summarize dataset info
         self.num_classes = self.dataTool.n_classes
         img_width, img_height, img_channels = self.dataTool.image_shape
         self.img_params = [img_width, img_height, img_channels]
+
+    def buildNetwork(self):
+        self.lenet = LeNet(self.num_classes, self.img_params)
+        self.lenet.build()
+
 
     def train(self):
         pass
@@ -33,7 +39,8 @@ def main():
     # run pipeline
     # output classification results
     data_dir = '../traffic-signs-data/'
-    traffic_sign_pipeline = Pipeline(data_dir)
+    visualize_dir =  '../visualize/'
+    traffic_sign_pipeline = Pipeline(data_dir, visualize_dir)
 
     traffic_sign_pipeline.exploreDataset()
 
--- P2_traffic_sign/code/utils.py
+++ P2_traffic_sign/code/utils.py
@@ -3,17 +3,18 @@ import pickle
 
 import numpy as np
 import matplotlib.pyplot as plt
-plt.switch_backend('agg')
+# plt.switch_backend('agg')
 from skimage import exposure
+from collections import defaultdict
 
 
 class DataSetTools():
     def __init__(self):
         print("Step 1: Dataset Summary & Exploration")
 
+        self.X_train_augment = None
 
     def loadData(self, data_dir):
-
         print("load trianing and validation data")
         training_file = data_dir + "train.p"
         validation_file = data_dir + "valid.p"
@@ -31,7 +32,6 @@ class DataSetTools():
         self.X_test, self.y_test = test['features'], test['labels']
 
     def summarizeData(self):
-
         self.n_train = self.X_train.shape[0]
         self.n_test = self.X_test.shape[0]
         self.image_shape = self.X_train[0].shape
@@ -41,6 +41,47 @@ class DataSetTools():
         print("Number of testing examples =", self.n_test)
         print("Image data shape =", self.image_shape)
         print("Number of classes =", self.n_classes)
+
+    def visualizeHistogram(self, labels, fileName, imgPath):
+        # histogram of classes
+        plt.close('all')
+        print("historgram bins arranged by classes ")
+        plt.hist(labels, bins=self.n_classes)
+        plt.title(fileName + "Data Histogram")
+        plt.xlabel("Class")
+        plt.ylabel("Occurence")
+        plt.savefig(imgPath + fileName + '_histogram.jpg')
+        # plt.show()
+
+    def visualizeUniqueImgs(self, labels, imgs, isGray, imgPath, fileName):
+        # plot unique images
+        numRows = 5
+        _, unique_indices = np.unique(labels, return_index=True)
+        unique_images = imgs[unique_indices]
+        fig = plt.figure()
+        for i in range(self.n_classes):
+            ax = fig.add_subplot(numRows, self.n_classes / numRows + 1, i + 1, xticks=[], yticks=[])
+            ax.set_title(i)
+            if (isGray == True):
+                ax.imshow(np.squeeze(unique_images[i]), cmap='gray')
+            else:
+                ax.imshow(unique_images[i])
+
+        plt.savefig(imgPath + fileName + '_sample')
+        plt.close('all')
+
+
+    def visualizeData(self, tag, imgPath):
+        # two options, training visualize and augmented data visualize
+        if tag == 'train':
+            isGray = False
+            imgs, labels, fileName = self.X_train, self.y_train, 'training'
+        elif tag == 'augment':
+            isGray = True
+            imgs, labels, fileName = self.X_train_augment, self.y_train, 'augmented'
+
+        self.visualizeHistogram(labels, fileName, imgPath)
+        self.visualizeUniqueImgs(labels, imgs, isGray, imgPath, fileName)
 
 class ImgPreprocess():
     def __init__(self):
